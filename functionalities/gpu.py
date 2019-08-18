0fef61d9b75f9940f6a3d98f24701fdd30519aa3
Xenovortex <Leo.Nguyen@gmx.de>
2019-08-18 21:06:15 +0200

    python file to choose gpu, if multiple gpus are available

---
+++ functionalities/gpu.py
@@ -0,0 +1,14 @@
+import torch
+
+def get_device(dev_idx=0):
+    """
+    Check if GPU is available and select GPU base on provided device index.
+
+    :param dev_idx: index of the GPU device to use (if your PC has multiple GPU's). Default: 0
+    :return: device variable used for further training on the GPU
+    """
+
+    torch.cuda.set_device(dev_idx)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   
+    return device
