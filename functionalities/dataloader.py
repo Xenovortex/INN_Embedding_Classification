00306752f90f1112396680cb534638c4b7eaa090
Xenovortex <Leo.Nguyen@gmx.de>
2019-08-20 00:25:35 +0200

    loading imagenet64 works now with tensor dataset

--- functionalities/dataloader.py
+++ functionalities/dataloader.py
@@ -1,8 +1,54 @@
+import os
 import torch
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+import torch.utils.data
 import pickle
-import urllib2
+import urllib.request as req
+import numpy as np
+from tqdm import tqdm_notebook as tqdm
+
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo)
+    return dict
+
+def load_databatch(datafolder, idx, img_size=64, mean=None):
+    """
+    Load data batch from downloaded imagenet64. If mean is provided, load testset and apply mean for normalization.
+    """
+    if mean is None:
+        datafile = os.path.join(datafolder, 'train_data_batch_')
+        d = unpickle(datafile + str(idx))
+        mean_image = d['mean']
+    else:
+        datafile = os.path.join(datafolder, 'val_data')
+        d = unpickle(datafile)
+        mean_image = mean
+
+    x = d['data']
+    y = d['labels']
+    
+    x = x/np.float32(255)
+    mean_image = mean_image/np.float32(255)
+
+    # Labels are indexed from 1, shift it so that indexes start at 0
+    y = [i-1 for i in y]
+    data_size = x.shape[0]
+
+    x -= mean_image
+
+    img_size2 = img_size * img_size
+
+    x = np.dstack((x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
+    x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
+
+    X = x[0:data_size, :, :, :]
+    Y = y[0:data_size]
+    
+    if mean is None:
+        return torch.Tensor(X), torch.Tensor(Y), mean_image
+    else:
+        return torch.Tensor(X), torch.Tensor(Y)
 
 
 def load_imagenet():
@@ -16,12 +62,21 @@ def load_imagenet():
     save_path = "./datasets/imagenet"
     class_dict_link = 'https://gist.githubusercontent.com/yrevar/6135f1bd8dcf2e0cc683/raw/d133d61a09d7e5a3b36b8c111a8dd5c4b5d560ee/imagenet1000_clsid_to_human.pkl'
 
-    transform = transforms.Compose([transforms.ToTensor()])
+    train_x = []
+    train_y = []
+    mean_image = None
+    for i in tqdm(range(1, 11)):
+        x, y, mean_image = load_databatch(save_path, idx=i)
+        train_x.append(x)
+        train_y.append(y)
 
-    trainset = datasets.ImageNet(root=save_path, split='train', download=True, transform=transform)
-    testset = datasets.ImageNet(root=save_path, split='val', download=True, transform=transform)
+    train_x, train_y = torch.cat(train_x, out=torch.Tensor(len(train_x), train_x[0].size()[0], train_x[0].size()[1], train_x[0].size()[2])), torch.cat(train_y, out=torch.Tensor(len(train_y)))
+    trainset = torch.utils.data.TensorDataset(train_x, train_y)
 
-    classes = pickle.load(urllib2.urlopen(class_dict_link))
+    test_x, test_y = load_databatch(save_path, idx=None, mean=mean_image)
+    testset = torch.utils.data.TensorDataset(test_x, test_y)
+
+    classes = pickle.load(req.urlopen(class_dict_link))
 
     return trainset, testset, classes
 
